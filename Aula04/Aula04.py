fb88f5f68a5b9204762852ef4a79b6110afae3ef
Flávio <flaviomarques2012@mgail.com>
2024-08-20 17:58:35 -0300

    salvo

---
+++ Aula04/Aula04.py
@@ -0,0 +1,40 @@
+# Primeiro algoritimo python
+
+numero1 = int(input('Informe o primeiro numero \n'))
+numero2 = int(input('Informe o primeiro numero \n'))
+
+# print(numero1 + numero2)
+# print(type(numero1))
+# print(type(numero2))
+
+
+# --> Tipos de Conversões 
+# int()
+# float()
+# str()
+# boot()
+
+
+# nome = "Flavio"  
+# idade = 31
+# altura = 1.65
+# vivo = True
+
+# print(type(nome))
+# print(type((idade)))
+# print(type(altura))
+# print(type(int(vivo)))
+
+
+# Operadores matematicos
+
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print('A soma é:', soma)
+print('A subtracao é: '+ str(subtracao))
+print('A mutiplicacao é: {} e a divisão foi {}'.format(multiplicacao, divisao))
+print(f'A mutiplicacao é: {multiplicacao} e a divisão foi {divisao}')
---
+++ Aula04/Aula5/aula05.py
@@ -0,0 +1,95 @@
+
+# numero1 = 10
+# numero2 = 10
+
+# # Operador maior
+# print(numero1 > numero2)
+# print(numero2 > numero1)
+
+# #operador menor
+# print (numero1 < numero2)
+# print (numero2 < numero1)
+
+# print(numero1 == numero2)
+
+# print(numero1 == numero2)
+# print(numero1 >= numero2)
+# print(numero1 <= numero2)
+# print(numero1 != numero1)
+
+#Variavéis = > <
+
+#int tipos inteiros
+#float
+# str
+#  
+
+
+# idade = int(input('Informe sua idade: \n'))
+
+# if (idade > 120):
+#     print('idade invalida')
+# elif(idade >= 18):
+#     print('maior de idade')
+#     elif (idade < 0):
+#         print('ainda não nasceu')
+# else:
+
+# Verdadeiro ou Falso, sim ou não
+
+
+# idade = int(input('Informe sua idade: \n'))
+
+# if (idade >= 18):
+#     print('Pode assitir o Filme')
+# elif(idade >= 16):
+#     acompanhado =input('Eta acompanhado de adulto SIM OU NÃO? \n')
+#     if (acompanhado == 'sim'):
+#       print('pode assistir')
+#     else:
+#         print('não pode assistir')
+# else:
+#     print('não pode assistir')
+
+
+# idade = int(input('Informe sua idade: \n'))
+
+# if(idade < 18):
+#    acompanhado =input('Eta acompanhado de adulto SIM OU NÃO? \n')
+#     if(acompanhado == 'sim'):
+#         print('pode assistir')
+#     else:
+#         print('pode assistir')
+#         else:
+#     print('não pode assistir')
+#     else:
+#         print('pode assitir')
+
+
+# usando variavel AND
+
+# alladin = input('Alladin apareceu? \n')
+# jasmine = input('jasmine apareceu? \n')
+
+# if (alladin == 'sim') and (jasmine =='sim'):
+#     print('Love a noite inteira')
+# else:
+#     print('não rolou encontro')
+
+
+
+# alladin = input ('Alladin apareceu? \n')
+# jasmine = input ('jasmine apareceu? \n')
+
+# if (alladin) == 'sim' or (jasmine == 'não')
+# print ('sim tever encontro')
+
+
+
+alladin = input('Alladin apareceu? \n')
+jasmine = input('jasmine apareceu? \n')
+
+if (alladin == 'sim') or (jasmine =='sim'):
+     print('Love a noite inteira com outra')
+else:
+     print('não rolou encontro')
