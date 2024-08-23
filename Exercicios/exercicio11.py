7bb4cd9f7c57b08156b7182d0c354c2e344091de
Flávio <flaviomarques2012@gmail.com>
2024-08-23 17:52:03 -0300

    atividade

--- Exercicios/exercicio11.py
+++ Exercicios/exercicio11.py
@@ -1,3 +1,17 @@
-#Escreva um programa que solicite ao usuário para digitar seu nome e, em seguida, exiba uma mensagem de boas-vindas personalizada.
+# # # Escreva um programa que solicite ao usuário para digitar seu nome e, em seguida, exiba uma mensagem de boas-vindas personalizada.
 
-nome = in(input('Digite seu nome:' ))
+# def boas_vindas():
+#     nome = input('Digite seu nome: ')
+#     print(f'Bem-Vindo(a), {nome}!')
+# boas_vindas()
+
+
+def boas_vindas():
+    # Solicita ao usuário que digite seu nome
+    nome = input("Por favor, digite seu nome: ")
+
+    # Exibe uma mensagem de boas-vindas personalizada
+    print(f"Bem-vindo(a), {nome}!")
+
+# Chama a função para executar o programa
+boas_vindas()
