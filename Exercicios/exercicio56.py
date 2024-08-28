01f8335ffa2968509c3f7ea4b57d9c68d2692525
Flávio <flaviomarques2012@gmail.com>
2024-08-28 17:56:26 -0300

    aula_28_08

---
+++ Exercicios/exercicio56.py
@@ -0,0 +1,14 @@
+# 56. Escreva um programa que pergunte ao usuário quantas vezes ele quer que uma mensagem seja exibida,
+#  e utilize um laço while para exibir a mensagem a quantidade de vezes desejada.
+
+
+
+
+
+
+
+contador = 0
+while (contador < 5):
+    contador
+else:
+    print('Perigo risco de encendio')  
